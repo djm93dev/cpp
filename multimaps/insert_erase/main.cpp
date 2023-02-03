675a10c102b0155254add6a8335e4617c8a8951e
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-03 15:03:52 -0500

    insert / erase from a map/multimaps

---
+++ multimaps/insert_erase/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main(){
+    multimap<int, string> this_map;
+    int temp;
+    string str;
+    pair<int, string> p;
+
+    cout << "Inserting...\n";
+    while(temp>=0){
+        cout << "Enter a number: ";
+        getline(cin, str);
+        temp = stoi(str);
+        if(temp>=0){
+            p.first = temp;
+            cout << "Enter a string: ";
+            getline(cin, str);
+            p.second = str;
+            this_map.insert(p);
+        }
+    }
+
+    cout << "Printing...\n";
+    for(auto it=this_map.begin(); it!=this_map.end(); it++)
+        cout << it->first << ":" << it->second << "  ";
+        
+    cout << endl;
+
+    temp=0;
+    cout << "Removing...\n";
+    while(temp>=0){
+        cout << "Enter a number: ";
+        getline(cin, str);
+        temp = stoi(str);
+        if(temp>=0)
+            this_map.erase(temp);
+    }
+
+    cout << "Printing...\n";
+    for(auto it=this_map.begin(); it!=this_map.end(); it++)
+        cout << it->first << ":" << it->second << "  ";
+
+
+    return 0;
+
+}
---
+++ unordered_sets/insert_erase/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
+int main(){
+    unordered_set<int> this_set;
+    int temp;
+
+    cout << "Inserting...\n";
+    while(temp>=0){
+        cout << "Enter a number: ";
+        cin >> temp;
+        if(temp>=0)
+            this_set.insert(temp);
+    }
+
+    temp=0;
+    cout << "Removing...\n";
+    while(temp>=0){
+        cout << "Enter a number: ";
+        cin >> temp;
+        if(temp>=0)
+            this_set.erase(temp);
+    }
+
+    cout << "Printing...\n";
+    for(auto it=this_set.begin(); it!=this_set.end(); it++)
+        cout << *it << " ";
+
+    return 0;
+}
