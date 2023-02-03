201173eaae8212c649117f82bce6a6644674db16
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-03 13:34:51 -0500

    birthday party example

---
+++ lists/push_back/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+void print(const list<int>& my_list) {
+    cout<<"list contains: { ";
+    for(const int& num: my_list)
+        cout<<num<<" ";
+    cout<<"}\n\n";
+
+}
+
+int main(){
+    list<int> numbers;
+    int value=0;
+
+    cout<<"Pushing back...\n";
+    while(value>=0){
+        cout<<"Enter a number: ";
+        cin>>value;
+        if(value>=0)
+            numbers.push_back(value);
+    }
+    print(numbers);
+    return 0;
+}
