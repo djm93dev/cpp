4bd1ddcc943ee1caeec85ec725ce24752bd80aa9
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-03 12:13:33 -0500

    push back

---
+++ push_back/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(const vector<int>& my_vector) {
+    cout<<"Vector contains: { ";
+    for(const int& num: my_vector)
+        cout<<num<<" ";
+    cout<<"}\n\n";
+
+}
+
+int main(){
+    vector<int> numbers;
+    int value=0;
+
+    cout<<"Pushing back...\n";
+    while(value>=0){
+        cout<<"Enter a number: ";
+        cin>>value;
+        if(value>=0)
+            numbers.push_back(value);
+    }
+    print(numbers);
+    return 0;
+}
