1a34d3f5dc6258785282a0f67c83250d426349fc
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-03 14:35:42 -0500

    visited urls example - added forward

--- usage_examples/visited_urls/main.cpp
+++ usage_examples/visited_urls/main.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main(){
     stack<string> back_stack;
+    stack<string> forward_stack;
     string temp;
 
     while(temp!="exit"){
-        cout << "[1] Visit a new URL    [2] Back" << endl;
+        cout << "[1] Visit a new URL    [2] Back    [3] Forward" << endl;
         cin >> temp;
         if(temp=="exit")
             break;
@@ -16,17 +17,29 @@ int main(){
             cout<<"Enter URL: ";
             cin>>temp;
             back_stack.push(temp);
+            forward_stack = stack<string>();
+
         }
         else if(temp=="2"){
             cout << "Going back... \n";
+            forward_stack.push(back_stack.top());
             back_stack.pop();
+        }
+        else if(temp=="3"){
+            if(forward_stack.empty())
+                cout << "No more URLs to visit forward" << endl;
+            else{
+                cout << "Going forward... \n";
+                back_stack.push(forward_stack.top());
+                forward_stack.pop();
+            }
         }
         else{
             cout << "Invalid input" << endl;
         }
         if (back_stack.empty())
             break;
-        cout << "Current URL: " << back_stack.top() << endl;
+        cout << "\nCurrent URL: " << back_stack.top() << endl;
     }
     return 0;
 }
