3c30bfa203f03773998b08aaad6d6b11ce4888f9
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-07 09:38:03 -0500

    macros as constants

---
+++ c_preprocessor/macros_as_constants/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+#define ONE 1 // no semi-colon at the end for macros
+#define HELLO "Hello World!"
+
+
+int main()
+{
+    printf("ONE = %d\n", ONE);
+    puts(HELLO);
+    return 0;
+}
