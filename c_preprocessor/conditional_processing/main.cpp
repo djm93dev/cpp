5f0dd0b3177e4b40973df4f176a038723000c14a
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-07 09:32:04 -0500

    conditional processing

---
+++ c_preprocessor/conditional_processing/main.cpp
@@ -0,0 +1,8 @@
+#ifndef CONDITIONAL_H_ // if not defined
+#define CONDITIONAL_H_ // define
+
+#define NUMBER 2 // define number
+
+#define CONSTANT 10 // define constant
+
+#endif /* CONDITIONAL_H_ */
