e22cc80e1b045318857745064fc778a453e95ab1
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-21 15:20:39 -0500

    validate user

--- regex/validation/main.cpp
+++ regex/validation/main.cpp
@@ -3,13 +3,24 @@
 #include <string>
 #include <regex>
 
+// User Class
+class User
+{
+public:
+    User(std::string email, std::string password, std::string birthday)
+        : email(email), password(password), birthday(birthday) {}
+
+    std::string email;
+    std::string password;
+    std::string birthday;
+};
+
+
 // validate email address
 bool validate_email(std::string email)
 {
-    // require 1 upper, 1 lower, 1 digit, 1 special, 8-20 chars
-    std::regex pattern("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,20})");
+    std::regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
     return std::regex_match(email, pattern);
-    
 }
 
 bool validate_password(std::string password)
@@ -18,31 +29,45 @@ bool validate_password(std::string password)
     return std::regex_match(password, pattern);
 }
 
+bool validate_birthday(std::string birthday)
+{
+    std::regex pattern("(\\d{2})/(\\d{2})/(\\d{4})");
+    return std::regex_match(birthday, pattern);
+}
+
 int main()
 {
-    std::string email;
-    std::cout << "Enter email address: ";
+    // ask user for email, password, birthday and store them in a User object
+    std::string email, password, birthday;
+    std::cout << "Enter email: ";
     std::cin >> email;
-    if (validate_email(email))
-    {
-        std::cout << "Valid email address" << std::endl;
-    }
-    else
-    {
-        std::cout << "Invalid email address" << std::endl;
-    }
-
-    std::string password;
     std::cout << "Enter password: ";
     std::cin >> password;
-    if (validate_password(password))
+    std::cout << "Enter birthday: ";
+    std::cin >> birthday;
+
+    // validate email, password, birthday
+    if (!validate_email(email))
     {
-        std::cout << "Valid password" << std::endl;
+        std::cout << "Invalid email address" << std::endl;
+        return 1;
     }
-    else
+    if (!validate_password(password))
     {
         std::cout << "Invalid password" << std::endl;
+        return 1;
     }
-
+    if (!validate_birthday(birthday))
+    {
+        std::cout << "Invalid birthday" << std::endl;
+        return 1;
+    }
+    // if all validations are passed, create a User object
+    User user(email, password, birthday);
+    std::cout << "User created successfully" << std::endl;
+    // print user info
+    std::cout << "Email: " << user.email << std::endl;
+    std::cout << "Password: " << user.password << std::endl;
+    std::cout << "Birthday: " << user.birthday << std::endl;
     return 0;
 }
