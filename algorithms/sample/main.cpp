48108e1927ed57fb30fde9a4571a3372ee978b92
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-22 12:04:30 -0500

    sample algo

---
+++ algorithms/sample/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <iterator>
+#include <algorithm>
+ 
+int main()
+{
+    std::string in = "ABCDEFGHIJK", out;
+    std::sample(in.begin(), in.end(), std::back_inserter(out), 4,
+                std::mt19937{std::random_device{}()});
+    std::cout << "five random letters out of " << in << " : " << out << '\n';
+}
