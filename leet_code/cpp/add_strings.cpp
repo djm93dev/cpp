a33be48db681377af61e3a6699bf33b27f1ed4fa
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-24 12:34:06 -0500

    add strings

---
+++ leet_code/cpp/add_strings.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+        string result = "";
+
+        while (i >= 0 || j >= 0 || carry == 1)
+        {
+            int sum = carry;
+            if (i >= 0)
+            {
+                sum += num1[i] - '0';
+                i--;
+            }
+            if (j >= 0)
+            {
+                sum += num2[j] - '0';
+                j--;
+            }
+            result = to_string(sum % 10) + result;
+            carry = sum / 10;
+        }
+        return result;
+    }
+};
+
+int main ()
+{
+    Solution s;
+    std::cout << s.addStrings("11", "123") << std::endl;
+    std::cout << s.addStrings("456", "77") << std::endl;
+    std::cout << s.addStrings("0", "0") << std::endl;
+    return 0;
+
+}
--- leet_code/cpp/excel_sheet_column_number.cpp
+++ leet_code/cpp/excel_sheet_column_number.cpp
@@ -12,7 +12,6 @@ public:
         int result = 0;
         for(char c : columnTitle)
         {
-			//d = s[i](char) - 'A' + 1 (we used  s[i] -  'A' to convert the letter to a number like it's going to be C)
             int d = c - 'A' + 1;
             result = result * 26 + d;
         }
