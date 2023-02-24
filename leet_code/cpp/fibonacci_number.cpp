b439fed9bb526bfb22b6b9bfdd630b1d8f4a31f2
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-24 14:24:44 -0500

    fibonacci number

---
+++ leet_code/cpp/fibonacci_number.cpp
@@ -0,0 +1,27 @@
+#include <numeric>
+#include <vector>
+#include <array>
+#include <iostream>
+#include <functional>
+#include <iterator>
+
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n-1) + fib(n-2);
+
+        
+    }
+};
+int main () {
+    Solution sol;
+    int n = 21;
+    int result = sol.fib(n);
+    cout << "Fibonacci number of " << n << " is " << result << endl;
+    return 0;
+}
+
