25e82b3d1580c2a0b244a9d1558865fa1076f0c8
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-27 15:08:53 -0500

    reverse prefix of word

--- leet_code/cpp/reverse_prefix_of_word.cpp
+++ leet_code/cpp/reverse_prefix_of_word.cpp
@@ -7,20 +7,22 @@ using namespace std;
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
+        if (word.find(ch) == string::npos) // if ch is not in word
+            return word; // return word
         string prefix;
-        for (int i = 0; i < word.size(); i++)
-            if (word[i] != ch)
-                prefix += word[i];
+        for (int i = 0; i < word.size(); i++) // for each char in word
+            if (word[i] != ch) // if char is not equal to ch
+                prefix += word[i]; // add char to prefix
             else {
-                prefix += word[i];
-                break;
+                prefix += word[i]; // add char to prefix
+                break; // break out of loop if char is equal to ch
             }
-        reverse(prefix.begin(), prefix.end());
-        for (int i = 0; i < prefix.size(); i++)
-            word[i] = prefix[i];
+        reverse(prefix.begin(), prefix.end()); // reverse prefix
+        for (int i = 0; i < prefix.size(); i++) // for each char in prefix
+            word[i] = prefix[i]; // replace char in word with char in prefix
 
         
-        return word;
+        return word; // return word
                 
         
     }
