32d7efb4844e404e0232b63c888999ba5019f1d0
Daniel McDonald <daniel@danielmcdonald.dev>
2023-02-27 09:18:07 -0500

    number of segments in a string

---
+++ leet_code/cpp/number_of_segments_in_a_string.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSegments(string s) {
+        int ans = 0;
+        for (int i = 0; i < s.size(); i++)
+            // check if the character is a space and the next character is not a space
+            if (s[i] == ' ' && s[i + 1] != ' ' && i != s.size() - 1)
+                ans++; // increment ans
+        if (s.size() == 0) // if the size of the string is 0, then return 0
+            return 0;
+        else if (s.size() == 1 && s[0] != ' ') // if the size of the string is 1 and the character is not a space, then return 1
+            return 1;
+        else if (s[0] != ' ') // if the first character is not a space, then increment ans
+            ans++;
+        return ans;
+
+            
+        
+    }
+};
+
+int main()
+{
+    string s = "Hello, my name is John";
+    Solution sol;
+    int ans = sol.countSegments(s);
+    cout << ans << endl;
+    return 0;
+}
