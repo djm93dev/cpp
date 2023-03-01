8e70d3210d5643b29fb3aff215f522ee3e50de65
Daniel McDonald <daniel@danielmcdonald.dev>
2023-03-01 15:35:06 -0500

    jump game

--- leet_code/cpp/jump_game.cpp
+++ leet_code/cpp/jump_game.cpp
@@ -5,11 +5,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int i = 0;
-        for (int reach = 0; i < n && i <= reach; ++i)
-            reach = max(i + nums[i], reach);
-        return i == n;
+        int n = nums.size(); // Get the size of the vector
+        int i = 0; // Create a variable to store the current index
+        for (int reach = 0; i < n && i <= reach; ++i) /// While the current index is less than the size of the vector and the current index is less than or equal to the reach
+            reach = max(i + nums[i], reach); // Set the reach to the maximum of the current index plus the number at the current index and the reach
+        return i == n; // Return whether the current index is equal to the size of the vector
     }
 };
 
