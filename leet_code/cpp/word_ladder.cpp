aa59cbdcc024e684675da50a58779937572635d9
Daniel McDonald <daniel@danielmcdonald.dev>
2023-03-02 15:35:59 -0500

    word ladder

--- leet_code/cpp/word_ladder.cpp
+++ leet_code/cpp/word_ladder.cpp
@@ -9,6 +9,36 @@ using namespace std;
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<string> q; // create a queue
+        q.push(beginWord); // push the begin word onto the queue
+        int level = 0; // set the level to 0
+        while (!q.empty()) { // while the queue is not empty
+            int size = q.size(); // set the size to the size of the queue
+            level++; // increment the level
+            for (int i = 0; i < size; i++) { // for the size of the queue
+                string word = q.front(); // set the word to the front of the queue
+                q.pop(); // pop the front of the queue
+                if (word == endWord) { // if the word is equal to the end word
+                    return level; // return the level
+                }
+                for (int j = 0; j < wordList.size(); j++) { // for the size of the word list
+                    if (wordList[j] == "") { // if the word list at j is equal to ""
+                        continue; // continue
+                    }
+                    int diff = 0; // set the diff to 0
+                    for (int k = 0; k < word.size(); k++) { // for the size of the word
+                        if (word[k] != wordList[j][k]) { // if the word at k is not equal to the word list at j at k
+                            diff++; // increment the diff
+                        }
+                    }
+                    if (diff == 1) { // if the diff is equal to 1
+                        q.push(wordList[j]); // push the word list at j onto the queue
+                        wordList[j] = ""; // set the word list at j to ""
+                    }
+                }
+            }
+        }
+        return 0; // return 0
         
     }
 };
